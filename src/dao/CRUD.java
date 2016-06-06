fa5a9b82fe0d6c5f304a536063ee5933434db5c1
Guilherme Pereira <guilherme.h12@gmail.com>
2016-06-06 13:44:42 -0300

    Refs T43 Função Atualizar
    Desensolvida a função atualizar informações, junto com essa função foi inserida a de busca e a função listToArray, que ajuda no preenchimento do JComboBox

--- src/dao/CRUD.java
+++ src/dao/CRUD.java
@@ -1,6 +1,8 @@
 package dao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -24,5 +26,59 @@ public class CRUD {
 	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        }
+	}
+	
+	public Bar buscar(String nome){
+		try{
+			stmt = (Statement) Conexao.con.createStatement();			
+			ResultSet rs = stmt.executeQuery("select * from bares where nome = '"+nome+"';");
+			
+			while(rs.next()){
+				String nomeB= rs.getString("nome");
+				String endereco = rs.getString("endereco");
+				String contato = rs.getString("contato");
+				String obs = rs.getString("obsBar");
+				return new Bar(nomeB,endereco,contato,obs);				
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public ArrayList<Bar> listToArray(){
+		ArrayList<Bar> bares = new ArrayList<>();		
+		try{
+			stmt = (Statement) Conexao.con.createStatement();			
+			ResultSet rs = stmt.executeQuery("select * from bares;");
+			
+			while(rs.next()){
+				String nomeB= rs.getString("nome");
+				String endereco = rs.getString("endereco");
+				String contato = rs.getString("contato");
+				String obs = rs.getString("obsBar");
+				bares.add(new Bar(nomeB, endereco, contato, obs));			
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return bares;
+	}
+	
+	public boolean atualizar(Bar bar){
+		try{
+			stmt = (Statement) Conexao.con.createStatement();
+			stmt.executeUpdate("UPDATE bares SET nome='"
+                    + bar.getNome() + "',endereco='"
+                    + bar.getEndereco()+ "',contato='"
+                    + bar.getContato() +"',obsBar='"
+                    + bar.getObsBar()+"' WHERE nome='" + bar.getNome() + "'");
+			return true;
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
