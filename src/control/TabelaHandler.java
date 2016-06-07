237c8be773d5f82a164a9f9cb6aa0a90201d5605
Guilherme Pereira <guilherme.h12@gmail.com>
2016-06-07 20:14:44 -0300

    Refs T48 Controle da Tabela
    Desensevolvido o controle da tabela,inserção das linhas

---
+++ src/control/TabelaHandler.java
@@ -0,0 +1,24 @@
+package control;
+
+import java.util.ArrayList;
+
+import view.TelaListaBar;
+import model.Bar;
+import dao.CRUD;
+
+public class TabelaHandler {
+	private TelaListaBar tela;
+
+	public TabelaHandler(TelaListaBar tela) {
+		super();
+		this.tela = tela;
+	}
+	
+	public void inserirLinha(){
+		ArrayList<Bar> bares = new CRUD().listToArray();
+		for(Bar bar : bares){
+			
+			tela.getModel().addRow(new Object[]{bar.getId(),bar.getNome(),bar.getEndereco(),bar.getContato(),bar.getObsBar()});
+		}
+	}
+}
