c68230c15acaf48497bc4be1417b0f403037f9d2
Guilherne <Guilherne@DESKTOP-JVCSLUV>
2016-06-21 16:08:54 -0300

    Refs T165 Visual da Tela de Dono de Bar
    plano de fundo da tela na cor preta
    plano de fundo dos botões na cor branca
    fonte Tahoma,negrito,18
    tooltip texts isneridos

--- src/view/TelaDono.java
+++ src/view/TelaDono.java
@@ -11,6 +11,8 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Color;
+import java.awt.Font;
 
 public class TelaDono extends JFrame {
 
@@ -25,24 +27,40 @@ public class TelaDono extends JFrame {
 		setResizable(false);
 		setTitle("CHAMADO DO BAR -  DONO DE BAR");
 
-		setBounds(100, 100, 334, 247);
+		setBounds(100, 100, 366, 247);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		cadButton = new JButton("Cadastrar Bar");
+		cadButton.setToolTipText("Cadastrar um novo bar");
+		cadButton.setFont(new Font("Tahoma", Font.BOLD, 18));
+		cadButton.setBackground(Color.WHITE);
 		cadButton.addActionListener(new TelaDonoHandler(this));
 		
 		atualizaButton = new JButton("Atualizar");
+		atualizaButton.setToolTipText("Atualize seu bar");
+		atualizaButton.setFont(new Font("Tahoma", Font.BOLD, 18));
+		atualizaButton.setBackground(Color.WHITE);
 		atualizaButton.addActionListener(new TelaDonoHandler(this));
 		
 		listaButton = new JButton("Listar");
+		listaButton.setToolTipText("Listar todos os bares");
+		listaButton.setFont(new Font("Tahoma", Font.BOLD, 18));
+		listaButton.setBackground(Color.WHITE);
 		listaButton.addActionListener(new TelaDonoHandler(this));
 		
-		removeButton = new JButton("Remover meu Bar");
+		removeButton = new JButton("Remover");
+		removeButton.setToolTipText("Remover seu bar");
+		removeButton.setFont(new Font("Tahoma", Font.BOLD, 16));
+		removeButton.setBackground(Color.WHITE);
 		removeButton.addActionListener(new TelaDonoHandler(this));
 		
 		sairButton = new JButton("Sair");
+		sairButton.setToolTipText("Sair do Programa");
+		sairButton.setFont(new Font("Tahoma", Font.BOLD, 18));
+		sairButton.setBackground(Color.WHITE);
 		sairButton.addActionListener(new TelaDonoHandler(this));
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
@@ -52,32 +70,32 @@ public class TelaDono extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addContainerGap()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(listaButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(cadButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 139, Short.MAX_VALUE))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(listaButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(cadButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(removeButton, GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
-								.addComponent(atualizaButton, GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(removeButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(atualizaButton, GroupLayout.DEFAULT_SIZE, 158, Short.MAX_VALUE)))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(108)
+							.addGap(122)
 							.addComponent(sairButton, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap())
+					.addContainerGap(79, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(cadButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
-						.addComponent(atualizaButton, GroupLayout.DEFAULT_SIZE, 67, Short.MAX_VALUE))
+						.addComponent(atualizaButton, GroupLayout.DEFAULT_SIZE, 67, Short.MAX_VALUE)
+						.addComponent(cadButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(removeButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(listaButton, GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(removeButton, GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE)
+						.addComponent(listaButton, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(sairButton, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-					.addGap(34))
+					.addGap(8))
 		);
 		contentPane.setLayout(gl_contentPane);
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
